257039407e303d59749be5d1a2658851eb598bb4
Piyush-1332 <43117334+Piyush-1332@users.noreply.github.com>
2018-09-25 10:29:29 +0530

    png added

--- script.js
+++ script.js
@@ -6,11 +6,3 @@ function myMap() {
     var marker = new google.maps.Marker({position: myCenter});
     marker.setMap(map)
   }
-// function myMap() {
-//   var myCenter = new google.maps.LatLng(28.488244,77.519301)
-//   var mapCanvas = document.getElementById("map")
-//   var mapOptions = {center: myCenter, zoom: 17}
-//   var map = new google.maps.Map(mapCanvas, mapOptions)
-//   var marker = new google.maps.Marker({position:myCenter})
-//   marker.setMap(map)
-// }
